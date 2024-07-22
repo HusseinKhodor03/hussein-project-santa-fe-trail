fb7674cdfb12bd808eeeb5e2b1128e0ad1509995
HusseinKhodor03 <hkhodor03@gmail.com>
2024-07-22 12:20:18 -0700

    Implemented the full Agent class

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import random
 from environment import (
     SantaFeEnvironment,
     WINDOW_WIDTH,
@@ -7,6 +8,9 @@ from environment import (
     ANT_STARTING_X,
     ANT_STARTING_Y,
     DIRECTION_RIGHT,
+    DIRECTION_LEFT,
+    DIRECTION_UP,
+    DIRECTION_DOWN,
 )
 from collections import deque
 
@@ -21,21 +25,69 @@ class Agent:
         self.epsilon = 0
         self.gamma = 0
         self.memory = deque(maxlen=MAX_MEMORY)
+        self.model = None
+        self.trainer = None
 
     def get_state(self, environment):
-        pass
+        direction_right = environment.direction == DIRECTION_RIGHT
+        direction_left = environment.direction == DIRECTION_LEFT
+        direction_up = environment.direction == DIRECTION_UP
+        direction_down = environment.direction == DIRECTION_DOWN
+
+        nearest_food_pellet = min(
+            environment.food_pellet_positions,
+            key=lambda food_pellet: abs(food_pellet[0] - environment.x)
+            + abs(food_pellet[1] - environment.y),
+        )
+        food_pellet_x, food_pellet_y = nearest_food_pellet
+
+        food_pellet_right = food_pellet_x > environment.x  # food pellet right
+        food_pellet_left = food_pellet_x < environment.x  # food pellet left
+        food_pellet_up = food_pellet_y < environment.y  # food pellet up
+        food_pellet_down = food_pellet_y > environment.y  # food pellet down
+
+        state = [
+            direction_right,
+            direction_left,
+            direction_up,
+            direction_down,
+            food_pellet_right,
+            food_pellet_left,
+            food_pellet_up,
+            food_pellet_down,
+        ]
+
+        return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, run):
-        pass
+        self.memory.append((state, action, reward, next_state, run))
 
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, runs = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, runs)
 
     def train_short_memory(self, state, action, reward, next_state, run):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, run)
 
     def get_action(self, state):
-        pass
+        self.epsilon = 80 - self.num_runs
+        final_move = [0, 0, 0]
+
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model.predict(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
 
 
 def train():
